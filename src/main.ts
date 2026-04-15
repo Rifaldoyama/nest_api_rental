273323b81c2d19c6de815e8956e2940b7aaf46d7
Rifaldo Pratama <rifaldop49@gmail.com>
2026-04-16 05:37:56 +0700

    fix: restore start script and add port logger

--- src/main.ts
+++ src/main.ts
@@ -29,5 +29,6 @@ async function bootstrap() {
   );
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
+  console.log(`🚀 Application is running on port: ${port}`);
 }
 bootstrap();
